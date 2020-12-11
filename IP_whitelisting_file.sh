b38e918f4de5dfc5d1678631adcf12a2abc3de3f
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2020-12-11 23:42:56 +0530

    Update IP_whitelisting_file.sh

--- IP_whitelisting_file.sh
+++ IP_whitelisting_file.sh
@@ -2,13 +2,13 @@ echo 'a is new Port Number'
 a=55
 echo $a
 echo 'b is New IP to Whitelist'
-b=192.1.2.3
+b='192.1.2.3'
 echo $b
 echo 'c is Old Port Number'
 c=1001
 echo $c
 echo 'd is Old IP to Blacklist'
-d=2.22.221.22/2
+d='2.22.221.22/2'
 echo $d
 echo ''
 echo ' '
