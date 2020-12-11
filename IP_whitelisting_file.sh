34fe591814e6572d6f8e2c0bdf09f77c96d8a98c
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2020-12-11 23:48:27 +0530

    Update IP_whitelisting_file.sh

--- IP_whitelisting_file.sh
+++ IP_whitelisting_file.sh
@@ -2,17 +2,14 @@ echo 'a is new Port Number'
 a=55
 echo $a
 echo 'b is New IP to Whitelist'
-b='192.1.2.3'
+b=19.1.2.3
 echo $b
 echo 'c is Old Port Number'
 c=1001
 echo $c
 echo 'd is Old IP to Blacklist'
-d='2.22.221.22/2'
+d=2.22.221.22/2
 echo $d
-echo ''
-echo ' '
-
 echo 'Updating the IP for WHITELIST'
 echo ' '
-sh /var/lib/jenkins/workspace/aws/AWS_Security_group_rule_update.sh $a $b $c $d
+sh /var/lib/jenkins/workspace/aws/AWS_Security_group_rule_update.sh $a '$b' $c '$d'
