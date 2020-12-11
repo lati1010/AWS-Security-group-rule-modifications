263602e778b16be9a5cfa3bef20fc456a05b8892
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2020-12-11 22:34:08 +0530

    Update AWS_Security_group_rule_update.sh

--- AWS_Security_group_rule_update.sh
+++ AWS_Security_group_rule_update.sh
@@ -1,3 +1,7 @@
+aws configure set aws_access_key_id AKIARH5575PV3JXQQ2EE
+aws configure set aws_secret_access_key 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
+aws configure set default.region ap-south-1
+aws configure set output json
 aws ec2 revoke-security-group-ingress --group-name default --protocol tcp --port $1 --cidr $2
 aws ec2 authorize-security-group-ingress --group-name default --protocol tcp --port $3 --cidr $4
 aws ec2 describe-security-groups --group-name default --output text
