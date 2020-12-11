56628851c70a69ac8eae8c63809392b9b9a53597
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2020-12-11 23:35:52 +0530

    Update AWS_Security_group_rule_update.sh

--- AWS_Security_group_rule_update.sh
+++ AWS_Security_group_rule_update.sh
@@ -2,11 +2,28 @@
 #aws configure set aws_secret_access_key yLS+NQc8yLwikkwQIhX24/xTq88PDTsPklbC3UBT
 #aws configure set default.region ap-south-1
 #aws configure set output json
+echo 'Authenticating with AWS'
+echo ''
+echo ' '
 sh awscreds.sh
+echo ''
+echo ' '
+echo 'Security group rules BEFORE updating in text format'
+echo ' '
 aws ec2 describe-security-groups --group-name default --output text
+echo ''
+echo ''
+echo 'ADDING NEW RULE'
+echo ''
 aws ec2 authorize-security-group-ingress --group-name default --protocol tcp --port $1 --cidr $2
+echo ''
+echo 'REMOVING OLD RULE'
 aws ec2 revoke-security-group-ingress --group-name default --protocol tcp --port $3 --cidr $4
+echo ' '
+echo ' '
+echo 'Security group rules AFTER updating in text format'
+echo ''
 aws ec2 describe-security-groups --group-name default --output text
-pwd
+echo ''
 date
 echo "SUCCESS LAVAKUMAR :) :)"
