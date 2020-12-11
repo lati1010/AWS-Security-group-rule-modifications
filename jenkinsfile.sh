7a32f03be39040b59d4bdd7a55b09f90b6c357be
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2020-12-11 21:20:49 +0530

    test3
    test3

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -1,7 +1,7 @@
 pipeline {
     agent any
     stages {
-        stage('AWS security rule update') {
+        stage('AWS security rule update test') {
             steps {
                 sh 'pwd'
                 sh 'AWS_Security_group_rule_update.sh 111 192.19.1.1/32	10 10.10.100.10/32
