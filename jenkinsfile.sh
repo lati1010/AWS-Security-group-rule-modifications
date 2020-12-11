00b14334e2f6f3d897a0f2784eae60d28aae7fee
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2020-12-11 20:48:26 +0530

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -4,7 +4,7 @@ pipeline {
         stage('AWS security rule update') {
             steps {
                 sh 'pwd'
-                sh AWS_Security_group_rule_update.sh
+                sh 'AWS_Security_group_rule_update.sh 111 192.19.1.1/32	10 10.10.10.10/32
                 sh 'date'
                 
             }
