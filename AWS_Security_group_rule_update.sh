0beb41b2179f58376781e7c67fe85ed61d37e30b
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2021-03-11 21:09:15 +0530

    Update AWS_Security_group_rule_update.sh

--- AWS_Security_group_rule_update.sh
+++ AWS_Security_group_rule_update.sh
@@ -1,5 +1,5 @@
-#aws configure set aws_access_key_id AKIARH5575PV3JXQQ2EE
-#aws configure set aws_secret_access_key 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
+#aws configure set aws_access_key_id *****************
+#aws configure set aws_secret_access_key ************************************
 #aws configure set default.region ap-south-1
 #aws configure set output json
 echo 'Authenticating With AWS'
